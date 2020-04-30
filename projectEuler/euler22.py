5e0c8d6ba3b51e03d20eff4482f531efc152040c
roperb <brianwroper@gmail.com>
2020-04-30 10:16:36 -0700

    new projects

--- projectEuler/euler22.py
+++ projectEuler/euler22.py
@@ -12,3 +12,36 @@ What is the total of all the name scores in the file?
 
 """
 
+def scoreletter(c):
+    letterscore = {
+        'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,
+        'H':8,'I':9,'J':10,'K':11,'L':12,'M':13,
+        'N':14,'O':15,'P':16,'Q':17,'R':18,'S':19,
+        'T':20,'U':21,'V':22,'W':23,'X':24,'Y':25,'Z':26
+    }
+    return letterscore[c]
+
+def scorename(name):
+    score = 0
+    for c in name:
+        score += scoreletter(c)
+    return score
+
+file = open("names.txt","r")
+namestring = file.readline()
+file.close()
+namelist = ((''.join(c for c in namestring if c not in '"')).split(","))
+namelist.sort()
+
+nameposition = range(1,len(namelist)+1)
+namescore = []
+for name in namelist:
+    namescore.append(scorename(name))
+
+total = 0
+for i in range(len(namescore)):
+    total += nameposition[i]*namescore[i]
+
+print(total)
+
+
